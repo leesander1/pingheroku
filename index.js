57c34c412e4ee66fc414b1b7d651e4c698c90ef6
Lee <lee@leesander.com>
2017-04-13 08:58:58 -0500

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+const cronJob = require('cron').CronJob;
+const request = require('request');
+
+new cronJob( '30 08 * * *', function(){
+  request('https://fast-harbor-58566.herokuapp.com/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body);
+  });
+},  null, true,'America/Chicago');
+
+new cronJob( '02 09 * * *', function(){
+  request('https://fast-harbor-58566.herokuapp.com/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body);
+  });
+},  null, true,'America/Chicago');
+
+new cronJob( '30 20 * * *', function(){
+  request('https://fast-harbor-58566.herokuapp.com/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body);
+  });
+},  null, true,'America/Chicago');

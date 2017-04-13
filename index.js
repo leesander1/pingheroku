03ee2ce39500bc279c0cf97ad244a4174c42ae2a
Lee <lee@leesander.com>
2017-04-13 09:34:27 -0500

    update

--- index.js
+++ index.js
@@ -2,30 +2,24 @@ const cronJob = require('cron').CronJob;
 const request = require('request');
 
 /**
-new cronJob( '06 09 * * *', function(){
-  request('https://fast-harbor-58566.herokuapp.com/', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('ping: https://fast-harbor-58566.herokuapp.com/');
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log('body:', body);
-  });
+new cronJob( '24 09 * * *', function(){
+    ping('https://fast-harbor-58566.herokuapp.com/');
 },  null, true,'America/Chicago');
 */
 
 new cronJob( '30 08 * * *', function(){
-  request('https://fast-harbor-58566.herokuapp.com/', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('ping: https://fast-harbor-58566.herokuapp.com/');
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log('body:', body);
-  });
+    ping('https://fast-harbor-58566.herokuapp.com/');
 },  null, true,'America/Chicago');
 
 new cronJob( '30 20 * * *', function(){
-  request('https://fast-harbor-58566.herokuapp.com/', function (error, response, body) {
+    ping('https://fast-harbor-58566.herokuapp.com/');
+},  null, true,'America/Chicago');
+
+ping = (http) => {
+  request(http, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
-    console.log('ping: https://fast-harbor-58566.herokuapp.com/');
+    console.log('ping: '+ http);
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     //console.log('body:', body);
   });
-},  null, true,'America/Chicago');
+};
